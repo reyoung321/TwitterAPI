c043902688d0c045cedbe99e7206ca88c2160f40
reyoung321 <reyoung63@gmail.com>
2020-01-10 17:37:48 +0545

    Done API for twitter

---
+++ routes/tweet.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const Tweet = require('../models/tweets');
+const router = express.Router();
+
+router.post("/add",(req,res,next) => {
+    Tweet.create({
+        tweet:req.body.tweet,
+        tweetImage:req.body.tweetImage,
+        username:req.body.username,
+        fullname:req.body.fullname,
+        like:req.body.like,
+        dislike:req.body.dislike,
+        comment:req.body.comment
+    }).then(() => {
+      
+        res.json({ status: "Tweet posted!"});
+    }).catch(next);
+});
+
+
+
+
+router.get("/tweetList", (req,res,next) => {
+    Tweet.find({}, function(err, users) {
+        var userMap = {};
+    
+        users.forEach(function(user) {
+          userMap[user._id] = user;
+        });
+    
+        res.send(userMap);  
+      });
+    
+})
+
+module.exports = router;
